5498523845870860a6be1407fcc361258f09eec9
Rian Engracia <engraciarian@gmail.com>
2023-12-21 12:37:17 +0800

    rename TItleUrlModel to RawNewsModel

---
+++ Model/RawNewsModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ExtractNews.Model
+{
+    public class RawNewsModel
+    {
+        public required string Title { get; set; }
+        public required Uri NewsUrl { get; set; }
+        public required Uri ImageUrl { get; set; }
+        public required string Section { get; set; }
+        public required string SubSection { get; set; }
+        public DateTime DateTimeUploaded { get; set; }
+    }
+}
